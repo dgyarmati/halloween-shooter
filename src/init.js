376dd8cb006af59e264ae8479685a86c1cd43951
dgyarmati <dpolya.w@gmail.com>
2019-10-28 22:11:20 +0100

    added cors hack

--- src/init.js
+++ src/init.js
@@ -1,3 +1,34 @@
+(function() {
+
+    function isSameOrigin(url) {
+        return (new URL(url)).origin === window.location.origin;
+    }
+
+    function needsCORS(url) {
+        // not sure all the URLs that should be checked for
+        return !isSameOrigin(url) && !url.startsWith("blob:") && !url.startsWith("data:");
+    }
+
+    const srcSetFn = Object.getOwnPropertyDescriptor(HTMLImageElement.prototype, 'src').set;
+
+    Object.defineProperty(HTMLImageElement.prototype, 'src', {
+        enumerable: true,
+        set: function(url) {
+            if (needsCORS(url)) {
+                // Set if not already set
+                if (this.crossOrigin !== undefined) {
+                    this.crossOrigin = '';
+                }
+            } else {
+                this.crossOrigin = undefined;
+            }
+            // Set the original attribute
+            srcSetFn.call(this, url);
+        },
+    });
+
+}());
+
 let gameStarted = false;
 let renderer;
 
