0551b32f2717d3af71441ff5d58195543020c4dc
dgyarmati <dpolya.w@gmail.com>
2019-10-28 22:16:13 +0100

    cors bugfix

--- src/init.js
+++ src/init.js
@@ -1,33 +1,70 @@
-(function() {
+var http = require('http'),
+    httpProxy = require('http-proxy');
+url = require('url');
+request = require('request');
 
-    function isSameOrigin(url) {
-        return (new URL(url)).origin === window.location.origin;
+function processRequest(req, res, proxy, new_req){
+    req.url = new_req.path
+    req.headers.host = new_req.host
+    res.oldWriteHead = res.writeHead;
+    res.writeHead = function(statusCode, headers) {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.oldWriteHead(statusCode, headers);
     }
-
-    function needsCORS(url) {
-        // not sure all the URLs that should be checked for
-        return !isSameOrigin(url) && !url.startsWith("blob:") && !url.startsWith("data:");
+    try{
+        proxy.proxyRequest(req, res, {
+            host: req.headers.host.split(":")[0],
+            port: parseInt(req.headers.host.split(":")[1]) || 80
+        });
+    }catch(err){
+        console.log(err);
     }
+}
 
-    const srcSetFn = Object.getOwnPropertyDescriptor(HTMLImageElement.prototype, 'src').set;
-
-    Object.defineProperty(HTMLImageElement.prototype, 'src', {
-        enumerable: true,
-        set: function(url) {
-            if (needsCORS(url)) {
-                // Set if not already set
-                if (this.crossOrigin !== undefined) {
-                    this.crossOrigin = '';
-                }
-            } else {
-                this.crossOrigin = undefined;
+httpProxy.createServer(function (req, res, proxy) {
+    var url_path = req.url.slice(1)
+    if(url_path == "crossdomain.xml"){
+        proxy.proxyRequest(req, res, {
+            host: '127.0.0.1',
+            port: 9000
+        });
+    }
+    else{
+        new_req = url.parse(url_path)
+        if(new_req.host == "avatars.io"){
+            request({
+                url: new_req.href,
+                followRedirect:false
+            }, function(error, response, body){
+                var final_req = url.parse(response.headers.location);
+                processRequest(req, res, proxy, final_req);
+            });
+        }
+        else{
+            if(new_req.host && new_req.host.match(/dgm59|cloudinary|twimg|instagram|facebook|fbcdn|distillery|qrserver|igcdn|naver/)){
+                processRequest(req, res, proxy, new_req);
+                console.log("allowed " + new_req.host)
+            }else{
+                console.log("disallowed " + new_req.host)
+                res.writeHead(400);
+                res.write('Not Allowed');
+                res.end();
             }
-            // Set the original attribute
-            srcSetFn.call(this, url);
-        },
-    });
+        }
+    }
+}).listen(8000);
 
-}());
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.write('<?xml version="1.0"?>\n<!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">\n<cross-domain-policy>\n<allow-access-from domain="*" />\n</cross-domain-policy>\n');
+    res.end();
+}).listen(9000);
+
+
+process.on('uncaughtException', function(e){
+    console.log(e)
+});
 
 let gameStarted = false;
 let renderer;
